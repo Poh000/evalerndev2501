51071254497c6a922cf99215a5c6848a0bce7ebe
Poh000 <bierman.noa3@gmail.com>
2025-04-02 14:33:35 +0200

    Modification script.sh

---
+++ systeme/script.sh
@@ -0,0 +1,13 @@
+echo "Ou voulez-vous enregistrer le projet ? (Chemin absolu ex : c:/dossier)"
+read directory
+echo "Quel est le nom de votre projet ?"
+read project
+
+cd $directory
+
+mkdir $project
+touch ./$project/index.html
+touch ./$project/style.css
+touch ./$project/README.md
+
+echo "Le projet a été ajouté avec succès"
